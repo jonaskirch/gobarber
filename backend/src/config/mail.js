8377c4527bed6bb350b43b129e130e6e6dc3a12e
Jonas Kirch <jonaskirch9@gmail.com>
2020-02-04 21:45:18 -0300

    add send mail

---
+++ backend/src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '950d64c12fe24d',
+    pass: 'a32de0434f37f9',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// amazon SES
