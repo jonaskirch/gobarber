e865b771f0fc0b786b815472dbf936cc2c11cb70
jonas <jonas@office.br>
2020-02-06 10:47:31 -0300

    - refactor multer filname to use promisify

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,17 +1,19 @@
 import multer from 'multer';
 import crypto from 'crypto';
 import { extname, resolve } from 'path';
+import { promisify } from 'util';
 
 export default {
   // cdn - content delivery network... amazon s3, digital ocean spaces
   storage: multer.diskStorage({
     destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
-    filename: (req, file, cb) => {
-      // refatorar para usar promisify
-      crypto.randomBytes(16, (err, res) => {
-        if (err) return cb(err);
+    filename: async (req, file, cb) => {
+      try {
+        const res = await promisify(crypto.randomBytes)(16);
         return cb(null, res.toString('hex') + extname(file.originalname));
-      });
+      } catch (err) {
+        return cb(err);
+      }
     },
   }),
 };
