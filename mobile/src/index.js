404d3afcc829f86f3e49bb4a88790df28ae141ab
Jonas Kirch <jonaskirch9@gmail.com>
2020-03-02 22:07:21 -0300

    add reactotron config + redux

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { StatusBar } from 'react-native';
+
+import './config/RectotronConfig';
 
 // import { Container } from './styles';
+import { store, persistor } from './store';
 import Routes from './routes';
 
-export default function src() {
-  return <Routes />;
+export default function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <Routes />
+        </PersistGate>
+      </Provider>
+    </>
+  );
 }
